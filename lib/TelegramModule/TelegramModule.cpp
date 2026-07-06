1f910cbd68026f04732200be7556490552e3a089
dilharajay <dilhara5287@gmail.com>
2026-07-06 07:08:28 +0530

    Optimize Telegram WiFiClientSecure memory buffer size

--- lib/TelegramModule/TelegramModule.cpp
+++ lib/TelegramModule/TelegramModule.cpp
@@ -8,6 +8,8 @@ TelegramModule::TelegramModule(ScaleDriver& scale, GasSensorModule& gasSensor, T
 void TelegramModule::begin(SettingsModule& settings) {
     _settings = &settings;
     client.setInsecure();  // production: use CA cert instead
+    client.setBufferSizes(1024, 1024); // Reduce BearSSL memory footprint
+    client.setTimeout(5000); // Prevent blocking indefinitely
     
     if (String(Config::TELEGRAM_BOT_TOKEN).length() > 0) {
         bot = new UniversalTelegramBot(Config::TELEGRAM_BOT_TOKEN, client);
